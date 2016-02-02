6321e3c35e656ccd68c8228b6d296ef527242b6e
suxindao <suxindao@126.com>
2016-02-03 02:07:03 +0800

    get back chapter 8's code

--- 精通AngularJS/1820OS_10_Code/01 - ngLocale and the locale service/locale.js
+++ 精通AngularJS/1820OS_10_Code/01 - ngLocale and the locale service/locale.js
@@ -1,7 +1,7 @@
 angular.module('locale', ['ngLocale'])
-  .controller('LocaleCtrl', function ($scope, $locale) {
+    .controller('LocaleCtrl', function($scope, $locale) {
 
-    $scope.localeid = $locale.id;
+        $scope.localeid = $locale.id;
 
-    $scope.months = $locale.DATETIME_FORMATS.MONTH;
-  });
+        $scope.months = $locale.DATETIME_FORMATS.MONTH;
+    });
--- 精通AngularJS/1820OS_11_Code/03 - tuning cpu - watchers - expensive computations/expensiveComputation.js
+++ 精通AngularJS/1820OS_11_Code/03 - tuning cpu - watchers - expensive computations/expensiveComputation.js
@@ -1,25 +1,25 @@
 angular.module('expensiveComputation', [])
-  .controller('ExpensiveComputationCtrl', function ($scope) {
-    $scope.name = 'World';
+    .controller('ExpensiveComputationCtrl', function($scope) {
+        $scope.name = 'World';
 
-    $scope.myComplexComputation = function () {
-      console.log('computing');
-      return $scope.name;
-    };
+        $scope.myComplexComputation = function() {
+            console.log('computing');
+            return $scope.name;
+        };
 
 
-    $scope.getName = function () {
-      return $scope.name;
-    };
-    $scope.getNameLog = function () {
-      console.log('getting name');
-      return $scope.name;
-    };
-  })
+        $scope.getName = function() {
+            return $scope.name;
+        };
+        $scope.getNameLog = function() {
+            console.log('getting name');
+            return $scope.name;
+        };
+    })
 
-  .filter('myComplexFilter', function () {
-    return function (input) {
-      console.log('filtering');
-      return input;
+.filter('myComplexFilter', function() {
+    return function(input) {
+        console.log('filtering');
+        return input;
     };
-  });
+});
