168031c1055fee486db2a34f95123ee2f201866e
suxindao <suxindao@126.com>
2016-02-05 22:02:47 +0800

    update chapter 04

--- 《Node.js+MongoDB+AngularJS Web 开发》/ch05/buffer_concat.js
+++
@@ -1,5 +0,0 @@
-var af = new Buffer("African Swallow?");
-var eu = new Buffer("European Swallow?");
-var question = new Buffer("Air Speed Velocity of an ");
-console.log(Buffer.concat([question, af]).toString());
-console.log(Buffer.concat([question, eu]).toString());
