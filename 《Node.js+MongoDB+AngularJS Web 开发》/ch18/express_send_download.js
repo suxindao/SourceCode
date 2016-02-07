92d305705a7c9c0c3463b50a3366c8f2ca3ed97d
suxindao <suxindao@126.com>
2016-02-08 02:50:47 +0800

    update more codes

--- 《Node.js+MongoDB+AngularJS Web 开发》/ch18/express_send_download.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var url = require('url');
-var app = express();
-app.listen(80);
-app.get('/download', function (req, res) {
-  res.sendfile('./views/word.docx', 'new.docx');
-});
--- 《Node.js+MongoDB+AngularJS Web 开发》/ch19/express_static.js
+++
@@ -1,5 +0,0 @@
-var express = require('express');
-var app = express();
-app.use('/', express.static('./static'), {maxAge: 60*60*1000});
-app.use('/images', express.static( '../images'));
-app.listen(80);
